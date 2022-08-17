2258501cf2d7b79549d26631cdf262e7d27bbbfd
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-08-17 16:46:41 +0300

    Try to prove reduction for 3-Coloring problem

--- code/propositions/reductions.py
+++ code/propositions/reductions.py
@@ -1,4 +1,4 @@
-# This file is part of the materials accompanying the book
+# This file is part of the materials accompanying the book 
 # "Mathematical Logic through Python" by Gonczarowski and Nisan,
 # Cambridge University Press. Book site: www.LogicThruPython.org
 # (c) Yannai A. Gonczarowski and Noam Nisan, 2017-2021
